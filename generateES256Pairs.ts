4d77a0ba62297af17fbea63a7dc742ed144c5c7f
JakeAve <jacobjavery@gmail.com>
2022-08-04 20:17:34 -0600

    improved output

--- generateES256Pairs.ts
+++ generateES256Pairs.ts
@@ -2,7 +2,9 @@ import { generateES256Pair } from "./mod.ts";
 
 for (let i = 0; i < 10; i++) {
   const { privKey, pubKey } = await generateES256Pair();
+  console.log("--------------------Private Key---------------------");
   console.log(JSON.stringify(privKey));
+  console.log("--------------------Public Key----------------------");
   console.log(JSON.stringify(pubKey));
-  console.log("----------------------------------------------------");
+  console.log("\n");
 }
